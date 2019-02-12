20accd38071da04ece5347730ccb9e3902ce8000
duanguang <492466509@qq.com>
2019-02-12 17:40:23 +0800

    feat: 新增支持雪碧图及样式自动生成

--- src/app2/index.js
+++ src/app2/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from '../components/Main';
-
+import '../app2/assets/css/sprites-generated.css'
 // Render the main component into the dom
 ReactDOM.render(
     <div>
         18888882222222211
+        <div className='w-icon w-icon-logo'></div>
         {/* <App /> */}
     </div>, document.getElementById('app'));
