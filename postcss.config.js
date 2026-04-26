44f8d83d49e306a0eb3560458b04872159593466
duanguang <duanguang@hoolinks.com>
2026-04-26 12:12:40 +0800

    fix: 优化 postcss 配置中 browsers 变量的引用方式
    - 代码影响：影响模块：postcss.config.js
    - 实现变化：将直接使用 browsers 变量替换为 overrideBrowserslist 选项，以增强配置的清晰性
    - 技术细节：使用 overrideBrowserslist 替代 autoprefixer 配置中的 browsers 参数

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,6 @@ var EConfig_1 = require("./libs/settings/EConfig");
 var browsers = EConfig_1.default.getInstance().postcss.autoprefixer.browsers;
 module.exports = {
     plugins: [
-        require('autoprefixer')({ browsers })
+        require('autoprefixer')({ overrideBrowserslist: browsers })
     ]
 };
