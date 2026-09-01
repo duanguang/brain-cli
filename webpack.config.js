a7e595d187bf812a7164275f8665ede466fcf1a9
duanguang <duanguang@hoolinks.com>
2026-09-01 10:37:29 +0800

    feat: webpack.config.js 双内核分发器（engine × NODE_ENV 四路路由）

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,16 @@
         if (v !== undefined) module.exports = v;
     }
     else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "./cfg/dev", "./cfg/dist"], factory);
+        define(["require", "exports", "./cfg/dev", "./cfg/dist", "./cfg/rspack/dev", "./cfg/rspack/dist", "./libs/utils/engine"], factory);
     }
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     const dev_1 = require("./cfg/dev");
     const dist_1 = require("./cfg/dist");
+    const rspack_dev_1 = require("./cfg/rspack/dev");
+    const rspack_dist_1 = require("./cfg/rspack/dist");
+    const resolveEngine = require("./libs/utils/engine").default;
     const path = require('path');
     /**
      * Build the webpack configuration
@@ -25,6 +28,10 @@
             }
             else {
                 env = process.env.NODE_ENV = 'dev';
+            }
+            // v3 双内核分发：engine × NODE_ENV 四路路由（webpack 链路零改动）
+            if (resolveEngine(eConfig) === 'rspack') {
+                return env === 'production' ? (0, rspack_dist_1.default)(eConfig) : (0, rspack_dev_1.default)(eConfig);
             }
             switch (env) {
                 case 'production':
